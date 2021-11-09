26a96538481f7f9736567c52764fd5963f0d1efe
许志斌 <470138890@qq.com>
2021-11-09 23:29:56 +0800

    gg

--- speedtestDotnet/Program.cs
+++ speedtestDotnet/Program.cs
@@ -5,7 +5,7 @@ Progress<HttpDownloadProgress> p =new(o => {
     Console.WriteLine(o.BytesReceived.ToString());
 });
 client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36");
- 
+
  await client.GetByteArrayAsync(new Uri("https://vipspeedtest8.wuhan.net.cn:8080/download"),p, CancellationToken.None);
  
 Console.Read();
